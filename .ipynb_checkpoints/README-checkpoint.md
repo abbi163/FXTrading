a307f68f30ac8e1ff5b5e533eda939c9b158f929
Abhijeet Anand <abbi163@gmail.com>
2023-04-17 01:34:27 +0900

    GRID X BOT with Anchoring System Added

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,39 @@
+
+### FXTrading
+Welcome to the FXTrading repository! This is a collection of trading strategies and tools for the foreign exchange (Forex) market.
+
+--------------------------------------------------------------------
+-----------------------------------------------------------------------
+
+#### GridFXTrading
+The GridFXTrading folder contains strategies and tools specifically designed for grid trading. Grid trading is a popular Forex trading strategy that involves placing orders at regular intervals above and below a predetermined price level. This can help traders take advantage of ranging markets and potentially profit in both up and down markets.
+
+#### ScalperFXTrading
+The ScalperFXTrading folder contains strategies and tools specifically designed for scalping. Scalping is a fast-paced trading style that involves making many trades over short time frames in order to take advantage of small price movements. This can be a challenging but potentially profitable trading style for experienced traders.
+
+#### TrendFXTrading
+The TrendFXTrading folder contains strategies and tools specifically designed for trend following. Trend following is a popular trading style that involves identifying and following the direction of a market trend. This can help traders capture large price movements and potentially profit in strong trending markets.
+
+#### SwingFXTrading
+The SwingFXTrading folder contains strategies and tools specifically designed for swing trading. Swing trading is a trading style that involves holding positions for several days to several weeks in order to capture medium-term price movements. This can be a good trading style for traders who prefer a more relaxed trading approach and have less time to actively manage their trades.
+
+#### NewsFXTrading
+The NewsFXTrading folder contains strategies and tools specifically designed for news trading. News trading involves trading around economic news releases and other major events that can cause significant price movements in the Forex market. This can be a challenging but potentially profitable trading style for traders who are able to quickly react to market news.
+
+#### ArbitrageFXTrading
+The ArbitrageFXTrading folder contains strategies and tools specifically designed for arbitrage trading. Arbitrage trading involves taking advantage of price discrepancies between different markets or assets in order to make a profit. This can be a complex trading style that requires advanced technical knowledge and access to multiple trading platforms.
+
+#### PositionFXTrading
+The PositionFXTrading folder contains strategies and tools specifically designed for position trading. Position trading involves holding positions for several months to several years in order to capture long-term price movements. This can be a good trading style for traders who prefer a more hands-off approach to trading and have a longer-term outlook.
+
+#### RangeFXTrading
+The RangeFXTrading folder contains strategies and tools specifically designed for range trading. Range trading involves identifying and trading within a price range that a market has been trading within for a period of time. This can be a good trading style for traders who prefer a more systematic approach to trading and have a good understanding of support and resistance levels.
+
+#### BreakoutFXTrading
+The BreakoutFXTrading folder contains strategies and tools specifically designed for breakout trading. Breakout trading involves identifying and trading the price breakouts of significant support and resistance levels. This can be a good trading style for traders who prefer a more proactive approach to trading and have good technical analysis skills.
+
+#### DayFXTrading
+The DayFXTrading folder contains strategies and tools specifically designed for day trading. Day trading involves opening and closing positions within the same trading day in order to capture short-term price movements. This can be a challenging but potentially profitable trading style for traders who are able to make quick decisions and manage their risk effectively.
+
+#### Conclusion
+We hope you find the resources in this repository helpful for your Forex trading journey. Please keep in mind that trading carries a risk of loss and is not suitable for everyone. Please do your own research and make your own trading decisions based on your personal financial situation and risk tolerance. Good luck and happy trading!
---
+++ GridFXTrading/Vault/GridBotX/BotXSell.py
@@ -0,0 +1,304 @@
+
+#property copyright "BOTXSELL"
+#property version   "000.001"
+#property strict
+
+
+
+extern double gLotSize = 0.1;          // Set the lot size of the order
+extern double gMaxSlippage = 10;       // Maximum slippage allowed while opening the trade
+extern double gTakeProfit = 100;        // Set the take profit in points
+extern double gStopLoss = 10000;       // Set the stop loss in points
+extern string gOrderSendComment = "SELL-i-BOT" ;    // Order Comment 
+extern int gMaxOrders = 18;            // Maximum allowed Open Order
+extern double gDrawdown = 1;           // allowed drawdown in percentage !!
+extern double gGridStep = 70;          // Grid Steps in Points !!
+extern double gAnchorPrice = 1;        // Trade will stop once Bid price above this !! 
+
+
+double gMagicNumber = 11;      // Magic number for BotX-Sell is 11
+int gAnchorFlag = 0 ;  // If AnchorFlag is 0, Trade will go on
+int gDecimalDigits ;
+
+class CTrade {
+private:
+    double lotSize;
+    double maxSlippage;
+    double takeProfit;
+    double stopLoss;
+    int magicNumber;
+    string orderSendComment;
+
+public:
+    CTrade(double lotSize, double maxSlippage, double takeProfit, double stopLoss, int magicNumber, string orderSendComment) {
+        this.lotSize = lotSize;
+        this.maxSlippage = maxSlippage;
+        this.takeProfit = takeProfit;
+        this.stopLoss = stopLoss;
+        this.magicNumber = magicNumber;
+        this.orderSendComment = orderSendComment;
+    }
+
+    int BuyNow() 
+    {
+        double takeProfit = Ask + this.takeProfit * Point; // Set the take profit gTakeProfit points above the Ask price
+        double stopLoss = Bid - this.stopLoss * Point; // Set the stop loss gStopLoss below Bid price
+
+        // Open a buy order with the specified lot size, entry price, and take profit
+        int ticket = OrderSend(Symbol(), OP_BUY, this.lotSize, Ask, this.maxSlippage, stopLoss, takeProfit, this.orderSendComment, this.magicNumber, 0, Blue);
+
+        if (ticket > 0) {
+            Print("Buy order opened successfully with ticket #", ticket);
+            return ticket;
+        } else {
+            Print("Failed to open buy order with error code #", GetLastError());
+            return 0;
+        }
+    }
+
+    int SellNow() 
+    {
+        double takeProfit = Bid - this.takeProfit * Point; // Set the take profit TakeProfit points below the Bid price
+        double stopLoss = Ask + this.stopLoss * Point; // Set the stop loss gStopLoss above Bid price
+
+        // Open a sell order with the specified lot size, entry price, take profit, and stop loss
+        int ticket = OrderSend(Symbol(), OP_SELL, this.lotSize, Bid, this.maxSlippage, stopLoss, takeProfit, this.orderSendComment, this.magicNumber, 0, Red);
+
+        if (ticket > 0) {
+            Print("Sell order opened successfully with ticket #", ticket);
+            return ticket;
+        } else {
+            Print("Failed to open sell order with error code #", GetLastError());
+            return 0;
+        }
+    }
+};
+
+CTrade Trade(gLotSize, gMaxSlippage, gTakeProfit, gStopLoss, gMagicNumber, gOrderSendComment);
+
+int OnInit()
+  {
+   gDecimalDigits = Digits() ;
+   return(INIT_SUCCEEDED);
+  }
+  
+
+void OnDeinit(const int reason)
+  {
+
+   
+  }
+  
+
+void OnTick()
+  { 
+      
+   if (IsNewCandle())
+      {
+      
+      if (gAnchorPrice > Ask)
+         {
+         gAnchorFlag = 1 ;
+         CloseOrders() ;
+         Comment(StringFormat("Anchor Price of %G have been Achieved. All Trades have been closed", gAnchorPrice));
+         } 
+      
+      if (gAnchorFlag == 0)
+         {
+      
+         // Total Open Order Count !! 
+         int OrderCount = CountBotOrders() ; 
+         
+         
+         if (OrderCount == 0)
+         {  
+   
+            int sellTicket = Trade.SellNow();
+   
+          }
+          
+          // elif Grid Condition
+         if (OrderCount > 0)
+         {  
+            int lastOrderType = getLastOrderType() ;
+            double lastOpenPrice = getLastOrderOpenPrice() ;
+            
+            GridOpen(lastOrderType, lastOpenPrice);
+            ModifyTP(lastOrderType,  gTakeProfit, gMagicNumber);
+         }
+         // -------------------- TODELETE --------------------
+         // int lastOrderType = getLastOrderType();
+         // double x= lastOpenPrice  ;
+         // double y = Ask ;
+         // double z = x - y ;
+         
+         // Comment(StringFormat("Last Order Open price :  %G , Ask Price: %G , Diff : % G",x, y, z * Point));
+         // --------------------------------------------------
+      } 
+       
+      // Check drawdown and close if function return true. 
+      bool dd = CheckDrawdown(gDrawdown);
+      if (dd == true) CloseOrders();
+ 
+    }
+  }
+
+bool IsNewCandle()
+  {
+   static datetime saved_candle_time;
+   if(Time[0] == saved_candle_time)
+      return false;
+   else
+      saved_candle_time = Time[0];
+   return true;
+  }
+
+
+int CountBotOrders() 
+{   
+    // CountBotOrders() returns the total number of orders opened by a bot, based on the  magic number 
+    int orderCount = 0;
+    for (int orderPosition = OrdersTotal() - 1; orderPosition >= 0; orderPosition--) 
+    {
+        OrderSelect(orderPosition, SELECT_BY_POS, MODE_TRADES);
+        if (OrderSymbol() != Symbol() || OrderMagicNumber() != gMagicNumber) continue;
+        if (OrderSymbol() == Symbol() && OrderMagicNumber() == gMagicNumber)
+        if (OrderType() == OP_SELL || OrderType() == OP_BUY) orderCount++;
+    }
+    return orderCount;
+}
+  
+bool CheckDrawdown(double drawdown)
+{  
+    // If true : then close all trade, 
+    // If false: continue the trade 
+    // Convert drawdown from % to decimal
+    double ddLimit = NormalizeDouble(drawdown/100, 4);
+    
+    double balance = AccountBalance();
+    double equity = AccountEquity();
+    
+    if (ddLimit > NormalizeDouble(1-(equity/balance), 4))
+        { return false; }
+    else 
+        { return true;}
+}
+
+void CloseOrders()
+{
+   RefreshRates();
+
+   for (int i = (OrdersTotal() - 1); i >= 0; i--)
+   {
+      if (OrderSelect(i, SELECT_BY_POS, MODE_TRADES) == false)
+      {
+         Print("ERROR - Unable to select the order - ", GetLastError());
+         break;
+      } 
+
+      bool res = false;
+      int Slippage = 5;
+      double BidPrice = MarketInfo(OrderSymbol(), MODE_BID);
+      double AskPrice = MarketInfo(OrderSymbol(), MODE_ASK);
+
+      if (OrderType() == OP_BUY)
+      {
+         res = OrderClose(OrderTicket(), OrderLots(), BidPrice, Slippage);
+      }
+      else if (OrderType() == OP_SELL)
+      {
+         res = OrderClose(OrderTicket(), OrderLots(), AskPrice, Slippage);
+      }
+      
+      if (res == false) Print("ERROR - Unable to close the order - ", OrderTicket(), " - ", GetLastError());
+   }
+}
+
+double getLastOrderOpenPrice()
+{
+for(int pos = OrdersTotal()-1; pos >= 0 ; pos--) 
+    if (OrderSelect(pos, SELECT_BY_POS) 
+        && OrderMagicNumber() == gMagicNumber 
+        && OrderSymbol() == Symbol())
+            { 
+                return NormalizeDouble(OrderOpenPrice(), 5) ;
+            }
+ return -1 ;
+}
+
+
+int getLastOrderType()
+{
+for(int pos = OrdersTotal()-1; pos >= 0 ; pos--) 
+    if (OrderSelect(pos, SELECT_BY_POS) 
+        && OrderMagicNumber() == gMagicNumber 
+        && OrderSymbol() == Symbol())
+            { 
+                return OrderType();
+            }
+return -1 ;
+} 
+
+void GridOpen(int lastOrderType, double lastOpenPrice)
+{
+    RefreshRates();
+    
+    if(lastOrderType == 0 && lastOpenPrice - Ask > gGridStep * Point)
+        {
+            int buyTicket = Trade.BuyNow();
+        }
+
+    if(lastOrderType == 1 && Bid - lastOpenPrice > gGridStep * Point)
+        {
+            int sellTicket = Trade.SellNow();
+        }
+}
+
+void ModifyTP(int orderType, double TakeProfit, double magicNumber)
+{  
+   RefreshRates();
+
+   double totalOpenPrice = 0.0;
+   int numOrders = 0;
+   double newTakeProfit ;
+   
+   for (int pos = OrdersTotal() - 1; pos >= 0; pos--)
+   {
+      if (OrderSelect(pos, SELECT_BY_POS, MODE_TRADES))
+      {
+         if (OrderMagicNumber() == magicNumber && OrderType() == orderType && OrderSymbol() == Symbol())
+         {
+            totalOpenPrice += OrderOpenPrice();
+            numOrders++;
+         }
+      }
+   }
+   
+
+      double averageOpenPrice = totalOpenPrice / numOrders;
+       
+      if (orderType == 0)
+          { newTakeProfit =  averageOpenPrice + TakeProfit * Point ; }
+      
+      if (orderType == 1)
+         { newTakeProfit =  averageOpenPrice - TakeProfit * Point ; }
+   
+    newTakeProfit = NormalizeDouble(newTakeProfit, gDecimalDigits) ;
+    
+    for (int i = OrdersTotal() - 1; i >= 0; i--)
+       {
+          if (OrderSelect(i, SELECT_BY_POS, MODE_TRADES))
+          {
+             if (OrderMagicNumber() == magicNumber && OrderType() == orderType && OrderSymbol() == Symbol())
+             {  
+             
+                RefreshRates();
+                bool result = OrderModify(OrderTicket(), OrderOpenPrice(), OrderStopLoss(), newTakeProfit, 0, Blue);
+                if (result == false)
+                {
+                   Print("Error modifying take profit for order ", OrderTicket(), ": ", GetLastError());
+                }
+             }
+          }
+       }
+}
